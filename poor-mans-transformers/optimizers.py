9ae46312c2e5527870eccc172a765d2e25e0906b
Gustavo Montamat <gmontamat@gmail.com>
2021-09-06 17:58:48 -0300

    Implement Adam

--- poor-mans-transformers/optimizers.py
+++ poor-mans-transformers/optimizers.py
@@ -1,10 +1,42 @@
+import numpy as np
+
+
 class Optimizer:
 
     def __init__(self):
         pass
 
+    def __call__(self, parameter: np.ndarray, grad: np.ndarray):
+        raise NotImplementedError()
+
 
 class Adam(Optimizer):
+    """
+    Reference:
+        - [Kingma et al., 2014](http://arxiv.org/abs/1412.6980)
+    """
 
-    def __init__(self):
+    def __init__(self, learning_rate: float = 0.001,
+                 beta1: float = 0.9, beta2: float = 0.999,
+                 epsilon: float = 1e-8):
         super().__init__()
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = epsilon
+        # Define momentum and velocity
+        self.m = None
+        self.v = None
+        self.t = 0
+
+    def __call__(self, parameter: np.ndarray, grad: np.ndarray):
+        assert parameter.shape == grad.shape
+        self.t += 1
+        if self.m is None:
+            self.m = np.zeros_like(parameter)
+            self.v = np.zeros_like(parameter)
+        self.m = self.beta1 * self.m + (1. - self.beta1) * grad
+        self.v = self.beta2 * self.v + (1. - self.beta2) * np.power(grad, 2)
+        m_hat = self.m / (1. - self.beta1 ** self.t)
+        v_hat = self.v / (1. - self.beta2 ** self.t)
+        return parameter - self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
