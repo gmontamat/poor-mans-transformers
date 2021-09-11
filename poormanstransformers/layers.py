Fix overflow and grad in LogSoftmax

poormanstransformers/layers.py
@@ -140,7 +140,7 @@ class ReLU(Activation):
 class Softmax(Activation):
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        # Shift to negatives to avoid overflow
+        # Shift x to [-inf., 0] to avoid overflow
         # aka. stable softmax
         e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
         return e_x / np.sum(e_x, axis=-1, keepdims=True)
@@ -153,12 +153,13 @@ class Softmax(Activation):
 class LogSoftmax(Activation):
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        return x - np.sum(x, axis=-1, keepdims=True)
+        # Shift x to [-inf., 0] to avoid overflow
+        x_shifted = x - np.max(x, axis=-1, keepdims=True)
+        return x_shifted - np.log(np.sum(np.exp(x_shifted), axis=-1, keepdims=True))
 
     def backward(self, x: np.ndarray, grad: np.ndarray) -> np.ndarray:
-        # TODO: check your lazy math!
         softmax = np.exp(self.forward(x))
-        return (1. - softmax) * grad
+        return grad + softmax / softmax.shape[0]
 
 
 class Dropout(Activation):

poormanstransformers/losses.py
@@ -32,14 +32,12 @@ class CategoricalCrossEntropy(Loss):
     def __call__(self, y: np.ndarray, y_hat: np.ndarray) -> Tuple[float, np.ndarray]:
         if self.from_logits:
             # y_hat here are log probabilities (logits)
-            # We can avoid having to use np.log
-            # LogSoftmax is also more numerically stable
-            loss = np.mean(-y_hat[y == 1.])
-            # TODO: check your lazy math!
-            grad = - y / y.shape[0]
+            # LogSoftmax is more numerically stable
+            loss = -np.mean(np.sum(y * y_hat, axis=-1))
+            grad = -y / y.shape[0]
         else:
-            y_hat = np.clip(y_hat, 1e-15, 1 - 1e-15)  # Avoid division by zero
-            loss = np.mean(np.sum(-y * np.log(y_hat), axis=-1))
+            y_hat = np.clip(y_hat, 1e-15, 1 - 1e-15)  # Avoid overflow
+            loss = -np.mean(np.sum(y * np.log(y_hat), axis=-1))
             grad = (y_hat - y) / y.shape[0]
         return float(loss), grad
 